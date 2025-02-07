26e6c87ccb8731bebc776bc397966e0ce53aa570
Gianluca Mardente <gianluca.mardente@gmail.com>
2025-02-07 16:11:17 +0100

    Add digest
    If global.useDigest is set, digest is used instead of tag
    
    The script get_digest.sh can be used to fetch digest for each image
    for a given tag.
    For instance
    
    ./get_digest.sh v0.46.1
    addon-controller: sha256:ddaeb145b380227c9d71124c8240413660a8d18ce6aafb33acaf30459574947f
    event-manager: sha256:e78ad8c5c20edf2f5ae68851c8b5ceac7ca0452bd98ae182f5e210e822b49fb3
    webhook-conversion: sha256:37950f0a06661558ede2d58216d232d3dee5361be36ac29bde13cb1c34089cb6
    register-mgmt-cluster: sha256:a426b6915389bc5ed137e03c5bad55f24727fa22ad897cd1fec81fbd5b36d1e7
    shard-controller: sha256:9206fbf14bec49414c8bc3a1aa92ab9bc85528a745b94fb1b8451df2665da368
    healthcheck-manager: sha256:9a6442be8d7be2de4ef0b0e7f3d9a050b4516e8bf12d10c6491115349312ade1
    sveltoscluster-manager: sha256:1cb2b903628a90aae74a7fecfd4a17fee21c60674160e8917dc875d24d4b0786
    access-manager: sha256:c7794284849869f58c8bbb4df0effa89b7a7ede4944ee13b59dcd1693f19b168
    classifier: sha256:15cfc305c1aa8b85d0301130216e4e65a1eefc24ea7fc94c8d8568aaabb8c16e
    
    When deploying drift-detection-manager and sveltos-agent, digest is always used

---
+++ scripts/get_digest.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# Function to get the image digest
+get_image_digest() {
+  base_name="$1"  # e.g., addon-controller
+  tag="$2"
+
+  # Check if both arguments are provided
+  if [ -z "$base_name" ] || [ -z "$tag" ]; then
+    echo "Usage: $0 <base_name> <tag>"
+    return 1
+  fi
+
+  image_name="docker.io/projectsveltos/$base_name:$tag"
+
+  digest=$(skopeo inspect --format '{{.Digest}}' "docker://$image_name" --override-os="linux" --override-arch="amd64" --override-variant="v8" 2>/dev/null)
+
+  if [ -z "$digest" ]; then
+    echo "Error: Could not retrieve digest for $image_name"
+    return 1
+  fi
+
+  echo "$digest"
+  return 0
+}
+
+# Main script
+if [ $# -ne 1 ]; then  # Check for exactly 1 argument, the tag
+  echo "Usage: $0 <tag>"
+  exit 1
+fi
+
+
+tag="$1"
+# Array of base names
+declare -a base_names=(
+  "addon-controller"
+  "event-manager"
+  "webhook-conversion"
+  "register-mgmt-cluster"
+  "shard-controller"
+  "healthcheck-manager"
+  "sveltoscluster-manager"
+  "access-manager"
+  "classifier"
+)
+
+# Loop through the base names
+for base_name in "${base_names[@]}"; do
+  digest=$(get_image_digest "$base_name" "$tag")
+
+  if [ $? -ne 0 ]; then
+    echo "Error getting digest for $base_name."
+    exit 1
+  fi
+
+  echo "$base_name: $digest"
+done
+
